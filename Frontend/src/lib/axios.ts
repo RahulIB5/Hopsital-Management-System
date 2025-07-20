760c446d15aace14c75d1eacb0e708eb14170e55
Rahul IB <rahulib7172@gmail.com>
2025-07-20 19:13:27 +0530

    ho ps ital type wise

--- Frontend/src/lib/axios.ts
+++ Frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../stores/authStore';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://hopsital-management-system.onrender.com/",
   headers: {
     'Content-Type': 'application/json',
   },
